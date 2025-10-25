0207fe10b015b80f5f4948845f5548724f9818ec
GoddayOkos @Ponzi <godday.okoduwa@fairmoney.io>
2025-10-25 21:07:15 +0100

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by FMY-545 on 25/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by FMY-545 on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
